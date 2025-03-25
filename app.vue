8b62268107e6b077bada52bb14fa87ea1baff174
Jessie981 <jessie.kuo@nextlink.com.tw>
2025-03-25 17:53:12 +0800

    Add example 1: Fetch data

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Nuxt 3 SSR Example</h1>
+    <p>這是一個伺服器端渲染的頁面。</p>
+    <p>API 資料：{{ data }}</p>
   </div>
 </template>
+
+<script setup>
+const { data } = await useFetch('/api/hello')
+</script>
